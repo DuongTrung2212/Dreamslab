cd87c23edfe95dd7dff40c1477ef1c766e710962
DuongTrung2212 <duongtrungqb12@gmail.com>
2025-11-18 11:59:52 +0700

    update

--- js/main.js
+++ js/main.js
@@ -26,8 +26,123 @@
     //         prevEl: ".swiper-button-prev",
     //     },
     // });
+    var loadProduct = function () {
+        const gridInitialItems = 6;
+        const listInitialItems = 3;
+        const gridItemsPerPage = 3;
+        const listItemsPerPage = 2;
+
+        let listItemsDisplayed = listInitialItems;
+        let gridItemsDisplayed = gridInitialItems;
+        let scrollTimeout;
+
+        function hideExtraItems(layout, itemsDisplayed) {
+            layout.find(".loadItem").each(function (index) {
+                if (index >= itemsDisplayed) {
+                    $(this).addClass("hidden");
+                }
+            });
+            if (layout.is("#listLayout")) updateLastVisible(layout);
+        }
+
+        function showMoreItems(layout, itemsPerPage, itemsDisplayed) {
+            const hiddenItems = layout.find(".loadItem.hidden");
+
+            setTimeout(function () {
+                hiddenItems.slice(0, itemsPerPage).removeClass("hidden");
+                if (layout.is("#listLayout")) updateLastVisible(layout);
+                checkLoadMoreButton(layout);
+            }, 600);
+
+            return itemsDisplayed + itemsPerPage;
+        }
+
+        function updateLastVisible(layout) {
+            layout.find(".loadItem").removeClass("last-visible");
+            layout
+                .find(".loadItem")
+                .not(".hidden")
+                .last()
+                .addClass("last-visible");
+        }
+        function checkLoadMoreButton(layout) {
+            if (layout.find(".loadItem.hidden").length === 0) {
+                if (layout.is("#listLayout")) {
+                    $("#loadMoreListBtn").hide();
+                    $("#infiniteScrollList").hide();
+                } else if (layout.is("#gridLayout")) {
+                    $("#loadMoreGridBtn").hide();
+                    $("#infiniteScrollGrid").hide();
+                }
+            }
+        }
+
+        hideExtraItems($("#listLayout"), listItemsDisplayed);
+        hideExtraItems($("#gridLayout"), gridItemsDisplayed);
+
+        $("#loadMoreListBtn").on("click", function () {
+            listItemsDisplayed = showMoreItems(
+                $("#listLayout"),
+                listItemsPerPage,
+                listItemsDisplayed
+            );
+        });
+
+        $("#loadMoreGridBtn").on("click", function () {
+            gridItemsDisplayed = showMoreItems(
+                $("#gridLayout"),
+                gridItemsPerPage,
+                gridItemsDisplayed
+            );
+        });
+
+        // Infinite Scrolling
+        function onScroll() {
+            clearTimeout(scrollTimeout);
+            scrollTimeout = setTimeout(function () {
+                const infiniteScrollList = $("#infiniteScrollList");
+                const infiniteScrollGrid = $("#infiniteScrollGrid");
+
+                if (
+                    infiniteScrollList.is(":visible") &&
+                    isElementInViewport(infiniteScrollList)
+                ) {
+                    listItemsDisplayed = showMoreItems(
+                        $("#listLayout"),
+                        listItemsPerPage,
+                        listItemsDisplayed
+                    );
+                }
+
+                if (
+                    infiniteScrollGrid.is(":visible") &&
+                    isElementInViewport(infiniteScrollGrid)
+                ) {
+                    gridItemsDisplayed = showMoreItems(
+                        $("#gridLayout"),
+                        gridItemsPerPage,
+                        gridItemsDisplayed
+                    );
+                }
+            }, 300);
+        }
+        function isElementInViewport(el) {
+            const rect = el[0].getBoundingClientRect();
+            return (
+                rect.top >= 0 &&
+                rect.left >= 0 &&
+                rect.bottom <=
+                    (window.innerHeight ||
+                        document.documentElement.clientHeight) &&
+                rect.right <=
+                    (window.innerWidth || document.documentElement.clientWidth)
+            );
+        }
+        $(window).on("scroll", onScroll);
+    };
 
     $(function () {
+        loadProduct();
         activeSearchOverlay();
         new Swiper(".mySwiper", {
             navigation: {
